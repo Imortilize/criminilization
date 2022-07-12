23d3e9076d69ec866c14426dfb6c800c77364df0
Dom Brittain <dom@hutchgames.com>
2022-07-12 01:47:17 +0100

    Stripped down the game of modules / menus

--- modules/disabled/tutorial/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tutorials` (
-    `T_id` int(11) NOT NULL AUTO_INCREMENT,
-    `T_module` varchar(75) NOT NULL,
-    `T_text` text NOT NULL,
-    PRIMARY KEY(`T_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
